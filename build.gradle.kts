f45bd2a0a5b6b6f1f76df1d793740f03a592b844
voidbert <humbertogilgomes@protonmail.com>
2025-04-04 23:31:11 +0100

    Add default Gradle Project

---
+++ build.gradle.kts
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2024 Humberto Gomes, José Lopes, Mariana Rocha
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+plugins {
+    application
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter:5.12.1")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.12.1")
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+application {
+    mainClass = "org.example.App"
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
