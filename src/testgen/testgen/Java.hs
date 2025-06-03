606f2888e79b331d35ff7fa054370a82d4daa243
Voidbert <humbertogilgomes@protonmail.com>
2025-06-03 01:18:40 +0100

    Add assertion code generation helpers

--- src/testgen/testgen/Java.hs
+++ src/testgen/testgen/Java.hs
@@ -1,7 +1,15 @@
 {-# LANGUAGE FlexibleInstances #-}
 
 -- | Java code generation utilities
-module Java (JavaData(..), indent, decorateTest) where
+module Java
+  (
+    JavaData(..)
+  , indent
+  , decorateTest
+  , assertTrue
+  , assertEquals
+  , assertSame
+  , assertThrows) where
 
 import Data.Char (isAscii, ord)
 import Data.List (intercalate)
@@ -125,3 +133,37 @@ decorateTest :: String   -- ^ Name of the test method
              -> [String] -- ^ Lines constituting the body of the test method
              -> [String] -- ^ Lines of the test method
 decorateTest name test = concat [["@Test", "void " ++ name ++ "() {"], indent test, ["}"]]
+
+-- Assertions
+
+-- | @assertTrue@ statement generator
+assertTrue :: String -- ^ A Java boolean expression
+           -> String -- ^ A Java assertion statement
+assertTrue c = "assertTrue(" ++ c ++ ");"
+
+-- | @assertEquals@ statement generator
+assertEquals :: String -- ^ A Java expression for the expected value
+             -> String -- ^ A Java expression for the obtained value
+             -> String -- ^ A Java assertion statement
+assertEquals e g = "assertEquals(" ++ e ++ ", " ++ g ++ ");"
+
+-- | @assertSame@ statement generator
+assertSame :: String -- ^ A Java expression for the expected value
+           -> String -- ^ A Java expression for the obtained value
+           -> String -- ^ A Java assertion statement
+assertSame e g = "assertSame(" ++ e ++ ", " ++ g ++ ");"
+
+-- | @assertThrows@ statement generator
+assertThrows :: String -- ^ Name of the expected exception class
+             -> [String] -- ^ Lines where the exception should occur
+             -> [String] -- ^ A Java assertion statement
+assertThrows e b =
+  [
+    "assertThrows("
+  , ("    " ++ e ++ ".class,")
+  , "    () -> {"
+  ] ++ (indent (indent b)) ++
+  [
+    "    }"
+  , ");"
+  ]
